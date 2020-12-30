b70f4a292e40675e33e7fcd7874fadceaa94d96b
sujata chandravanshi <sujatachandravanshi@gmail.com>
2020-12-31 03:18:29 +0530

    next

---
+++ super-stylist/src/components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Link,Route,BrowserRouter,Switch} from 'react-router-dom';
+import Login from './navbar/login';
+import Contact from './navbar/contact';
+import About from './navbar/about';
+import MainPage from './Main/mainpage';
+import FashionTips from './options/tips'
+import SlideShow from './home files/slideshow';
+import MenCard from './options/menCard';
+import WomenCard from './options/womenCard'
+class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleMouseHoverMen = this.handleMouseHoverMen.bind(this);
+        this.handleMouseHoverWomen = this.handleMouseHoverWomen.bind(this);
+        this.state = {
+          isHoveringMen: false,
+          isHoveringWomen:false,
+        };
+      }
+    
+      handleMouseHoverMen() {
+        this.setState(this.toggleHoverStateMen);
+      }
+      handleMouseHoverWomen() {
+        this.setState(this.toggleHoverStateWomen);
+      }
+
+    
+      toggleHoverStateMen(state) {
+        return {
+          isHoveringMen: !state.isHoveringMen,
+        };
+      }
+      toggleHoverStateWomen(state) {
+        return {
+          isHoveringWomen: !state.isHoveringWomen,
+        };
+      }
+
+    
+    render()
+    {
+    return <>
+    <BrowserRouter>
+    
+    
+    <Switch>
+        <Route exact path="/"><MainPage handleMouseHoverMen={this.handleMouseHoverMen} handleMouseHoverWomen={this.handleMouseHoverWomen}/><SlideShow/>{this.state.isHoveringMen && <MenCard/>}{this.state.isHoveringWomen && <WomenCard/>}</Route>
+        <Route exact path="/login"><Login/></Route>
+        <Route exact path="/About"><About/></Route>
+        <Route exact path="/Contact"><Contact/></Route>
+        <Route exact path="/FashionTips"><FashionTips/></Route>
+        <Route exact path="/Men"><MenCard/></Route>
+        <Route exact path="/Women"><Login/></Route>
+    </Switch>
+    </BrowserRouter>
+
+</>
+}
+
+}
+
+export default Home;
