674090129f999507d84f9fdddc355c243ae45cf9
sujata chandravanshi <sujatachandravanshi@gmail.com>
2021-01-07 03:38:35 +0530

    next

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,7 +3,7 @@ const app=require('./app');
 const connectDb=require('./connector');
  connectDb();
 
-const port=3000;
+const port=9999;
 const server=http.createServer(app);
 
 server.listen(port);
--- Backend/src/routes/product.js
+++ Backend/src/routes/product.js
@@ -27,6 +27,52 @@ router.post('/',(req,res)=>{
     .catch(err=>console.log(err));
 });
 
+router.get('/men_summer', async(req,res)=>{
+    const result=await Product.find({"gender":"male"} && {"product_type":"summer"});
+    res.send(result);
+});
+
+router.get('/men_winter', async(req, res)=>{
+    const result=await Product.find({"gender":"male"} && {"product_type":"winter"});
+    res.send(result);
+})
+
+router.get('/men_office', async(req, res)=>{
+    const result=await Product.find({"gender":"male"} && {"product_type":"office"});
+    res.send(result);
+})
+
+router.get('/men_wedding', async(req, res)=>{
+    const result=await Product.find({"gender":"male"} && {"product_type":"wedding"});
+    res.send(result);
+})
+
+router.get('/women_winter', async(req, res)=>{
+    const result=await Product.find({"gender":"female"} && {"product_type":"winter"});
+    res.send(result);
+})
+
+router.get('/women_summer', async(req,res)=>{
+    const result=await Product.find({"gender":"female"} && {"product_type":"summer"});
+    res.send(result);
+});
+
+router.get('/women_office', async(req, res)=>{
+    const result=await Product.find({"gender":"female"} && {"product_type":"office"});
+    res.send(result);
+})
+
+router.get('/women_wedding', async(req, res)=>{
+    const result=await Product.find({"gender":"female"} && {"product_type":"wedding"});
+    res.send(result);
+})
+
+router.get('/:_id',async(req,res)=>{
+    const result=await Product.find({"_id":req.params._id});
+    res.send(result);
+})
+
+
 module.exports=router;
 
 
